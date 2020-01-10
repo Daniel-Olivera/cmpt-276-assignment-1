cd4d6975d3751179dcab7feb549a40ff8786ef2f
Daniel <oliverad63@gmail.com>
2020-01-10 10:49:20 -0800

    Added UI package and class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String args[]) {
+        LensManager manager = new LensManager();
+        CameraTextUI ui = new CameraTextUI(manager);
+        ui.show();
+    }
 }
